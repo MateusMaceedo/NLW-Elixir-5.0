b16fd740965b26aeac059f370775a17cb86284ec
Mateus Macedo <63172367+MateusMaceedo@users.noreply.github.com>
2021-04-28 22:14:54 -0300

    Add files via upload

---
+++ test/inmana_web/views/error_view_test.exs
@@ -0,0 +1,17 @@
+defmodule InmanaWeb.ErrorViewTest do
+
+  # Build da aplication
+  use InmanaWeb.ConnCase, async: true
+
+  # Bring render/3 and render_to_string/3 for testing custom views
+  import Phoenix.View
+
+  test "renders 404.json" do
+    assert render(InmanaWeb.ErrorView, "404.json", []) == %{errors: %{detail: "Not Found"}}
+  end
+
+  test "renders 500.json" do
+    assert render(InmanaWeb.ErrorView, "500.json", []) ==
+             %{errors: %{detail: "Internal Server Error"}}
+  end
+end
